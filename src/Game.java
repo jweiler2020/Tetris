df557d149908d36118005968000993978598b9c3
jweiler2020 <jweiler2020@cusd.claremont.edu>
2018-02-22 13:31:33 -0800

    Changed imports in Game.java

---
+++ src/Game.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Game extends JPanel
+	implements ActionListener
+{
+	private Difficulty d;
+
+	public enum Difficulty {
+		EASY,
+		MEDIUM,
+		HARD
+	}
+
+	public Game(Difficulty d)
+	{
+
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+
+	}
+}
